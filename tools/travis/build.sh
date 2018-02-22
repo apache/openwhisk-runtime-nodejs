1b3c79b8637730687b9d6885f8f743f1d70a34ad
Chetan Mehrotra <chetanm@adobe.com>
2018-02-22 18:19:51 +0530

    Switch to Maven based dependency
    This is required for apache/incubator-openwhisk#3277

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -11,14 +11,6 @@ export OPENWHISK_HOME=$WHISKDIR
 
 IMAGE_PREFIX="testing"
 
-# Build runtime
-cd $ROOTDIR
-TERM=dumb ./gradlew \
-:core:nodejs6Action:distDocker \
-:core:nodejs8Action:distDocker \
--PdockerImagePrefix=${IMAGE_PREFIX}
-
-
 # Build OpenWhisk
 cd $WHISKDIR
 
@@ -30,6 +22,15 @@ docker tag openwhisk/invoker ${IMAGE_PREFIX}/invoker
 docker pull openwhisk/nodejs6action
 docker tag openwhisk/nodejs6action ${IMAGE_PREFIX}/nodejs6action
 
+TERM=dumb ./gradlew \
+:common:scala:install \
+:core:controller:install \
+:core:invoker:install \
+:tests:install
 
-
-
+# Build runtime
+cd $ROOTDIR
+TERM=dumb ./gradlew \
+:core:nodejs6Action:distDocker \
+:core:nodejs8Action:distDocker \
+-PdockerImagePrefix=${IMAGE_PREFIX}
