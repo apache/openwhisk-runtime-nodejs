19c536281a49f98166b1037e286904167a6ef121
Jonathan Springer <jonpspri@gmail.com>
2018-03-11 20:39:45 -0400

    Add maven build to build.sh

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -11,14 +11,6 @@ export OPENWHISK_HOME=$WHISKDIR
 
 IMAGE_PREFIX="testing"
 
-# Build runtime
-cd $ROOTDIR
-TERM=dumb ./gradlew \
-:core:nodejs6Action:dockerBuildImage \
-:core:nodejs8Action:dockerBuildImage \
--PdockerImagePrefix=${IMAGE_PREFIX}
-
-
 # Build OpenWhisk
 cd $WHISKDIR
 
@@ -29,3 +21,16 @@ docker pull openwhisk/invoker
 docker tag openwhisk/invoker ${IMAGE_PREFIX}/invoker
 #docker pull openwhisk/nodejs6action
 #docker tag openwhisk/nodejs6action ${IMAGE_PREFIX}/nodejs6action
+
+TERM=dumb ./gradlew \
+:common:scala:install \
+:core:controller:install \
+:core:invoker:install \
+:tests:install
+
+# Build runtime
+cd $ROOTDIR
+TERM=dumb ./gradlew \
+:core:nodejs6Action:dockerBuildImage \
+:core:nodejs8Action:dockerBuildImage \
+-PdockerImagePrefix=${IMAGE_PREFIX}
