090ffa65411bed792e03bcb8807187d9e6879133
Carlos Santana <csantanapr@apache.org>
2018-06-29 16:31:48 -0400

    clean up build and deploy

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -27,33 +27,16 @@ UTILDIR="$ROOTDIR/../incubator-openwhisk-utilities"
 
 export OPENWHISK_HOME=$WHISKDIR
 
-IMAGE_PREFIX="testing"
-
-# run scancode using the ASF Release configuration
+# Run scancode using the ASF Release configuration
 cd $UTILDIR
 scancode/scanCode.py --config scancode/ASF-Release.cfg $ROOTDIR
 
-# Build OpenWhisk
-cd $WHISKDIR
-
-#pull down images
-docker pull openwhisk/controller
-docker tag openwhisk/controller ${IMAGE_PREFIX}/controller
-docker pull openwhisk/invoker
-docker tag openwhisk/invoker ${IMAGE_PREFIX}/invoker
-docker pull openwhisk/nodejs6action
-docker tag openwhisk/nodejs6action ${IMAGE_PREFIX}/nodejs6action
-
-TERM=dumb ./gradlew \
-:common:scala:install \
-:core:controller:install \
-:core:invoker:install \
-:tools:admin:install \
-:tests:install
+# Build OpenWhisk deps
+cd $OPENWHISK_HOME
+TERM=dumb ./gradlew install
 
 # Build runtime
 cd $ROOTDIR
 TERM=dumb ./gradlew \
 :core:nodejs6Action:distDocker \
-:core:nodejs8Action:distDocker \
--PdockerImagePrefix=${IMAGE_PREFIX}
+:core:nodejs8Action:distDocker
