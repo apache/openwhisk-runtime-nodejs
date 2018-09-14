3af066efdc1e21fd35490ff61829c9d70d47750e
Carlos Santana <csantanapr@apache.org>
2018-09-14 15:05:32 -0400

    push a hashcommit of the image in travis publish (#89)
    Closes #88

--- tools/travis/publish.sh
+++ tools/travis/publish.sh
@@ -46,4 +46,16 @@ TERM=dumb ./gradlew \
 -PdockerRegistry=docker.io \
 -PdockerImagePrefix=${IMAGE_PREFIX} \
 -PdockerImageTag=${IMAGE_TAG}
+
+  # if doing latest also push a tag with the hash commit
+  if [ ${IMAGE_TAG} == "latest" ]; then
+  SHORT_COMMIT=`git rev-parse --short HEAD`
+  TERM=dumb ./gradlew \
+  :core:${RUNTIME}:distDocker \
+  -PdockerRegistry=docker.io \
+  -PdockerImagePrefix=${IMAGE_PREFIX} \
+  -PdockerImageTag=${SHORT_COMMIT}
+  fi
+
+
 fi
