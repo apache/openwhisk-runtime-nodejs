d967ebe339256979b759bd2bc37fe3cb6d8daff0
Carlos Santana <csantanapr@apache.org>
2018-10-30 16:41:05 -0400

    serialize errors (#97)
    * serialize errors
    Fixes #63
    Fixes #96
    
    * add serialize-error in the correct place duh!

--- core/nodejsActionBase/runner.js
+++ core/nodejsActionBase/runner.js
@@ -25,6 +25,7 @@ var util = require('util');
 var child_process = require('child_process');
 var fs = require('fs');
 var path = require('path');
+const serializeError = require('serialize-error');
 
 function NodeActionRunner() {
     // Use this ref inside closures etc.
@@ -114,11 +115,7 @@ function NodeActionRunner() {
                     if (!error) {
                         resolve({ error: {}});
                     } else {
-                        // Log stack trace for rejected promises.
-                        if (error.message) {
-                            console.error(error)
-                        }
-                        resolve({ error: error.toString() });
+                        resolve({ error: serializeError(error) });
                     }
                 });
             }
