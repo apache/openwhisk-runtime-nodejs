ddff7b8575de7295ff8b3c4fa678722f8d6f0be8
Priti Desai <pdesai@us.ibm.com>
2019-04-17 16:45:19 -0700

    changes to service.js

--- core/nodejsActionBase/src/service.js
+++ core/nodejsActionBase/src/service.js
@@ -24,12 +24,12 @@ function NodeActionService(config) {
         ready: 'ready',
         starting: 'starting',
         running: 'running',
-        stopped: 'stopped'
+        stopped: 'stopped',
     };
 
     // TODO: save the entire configuration for use by any of the route handlers
     var status = Status.ready;
-    var ignoreRunStatus = config.allowConcurrent === undefined ? false : config.allowConcurrent.toLowerCase() === "true";
+    var ignoreRunStatus = config.allowConcurrent === undefined ? false : config.allowConcurrent.toLowerCase() === 'true';
     var server = undefined;
     var userCodeRunner = undefined;
 
@@ -47,13 +47,22 @@ function NodeActionService(config) {
      * { code: int, response: object }
      *
      */
-    function responseMessage (code, response) {
+    function responseMessage(code, response) {
         return { code: code, response: response };
     }
 
-    function errorMessage (code, errorMsg) {
+    function errorMessage(code, errorMsg) {
         return responseMessage(code, { error: errorMsg });
     }
+
+    /**
+     * Indicates if we have been initialized which is determined by if we have
+     * created a NodeActionRunner.
+     * @returns {boolean}
+     */
+    this.initialized = function isInitialized(){
+        return (typeof userCodeRunner !== 'undefined');
+    };
 
     /**
      * Starts the server.
@@ -84,26 +93,26 @@ function NodeActionService(config) {
             var message = body.value || {};
 
             if (message.main && message.code && typeof message.main === 'string' && typeof message.code === 'string') {
-                return doInit(message).then(function (result) {
+                return doInit(message).then(function(result) {
                     setStatus(Status.ready);
                     return responseMessage(200, { OK: true });
-                }).catch(function (error) {
+                }).catch(function(error) {
                     setStatus(Status.stopped);
-                    var errStr = "Initialization has failed due to: " + error.stack ? String(error.stack) : error;
+                    var errStr = 'Initialization has failed due to: ' + error.stack ? String(error.stack) : error;
                     return Promise.reject(errorMessage(502, errStr));
                 });
             } else {
                 setStatus(Status.ready);
-                var msg = "Missing main/no code to execute.";
+                var msg = 'Missing main/no code to execute.';
                 return Promise.reject(errorMessage(403, msg));
             }
         } else if (userCodeRunner !== undefined) {
-            var msg = "Cannot initialize the action more than once.";
-            console.error("Internal system error:", msg);
+            var msg = 'Cannot initialize the action more than once.';
+            console.error('Internal system error:', msg);
             return Promise.reject(errorMessage(403, msg));
         } else {
-            var msg = "System not ready, status is " + status + ".";
-            console.error("Internal system error:", msg);
+            var msg = 'System not ready, status is ' + status + '.';
+            console.error('Internal system error:', msg);
             return Promise.reject(errorMessage(403, msg));
         }
     };
@@ -122,23 +131,23 @@ function NodeActionService(config) {
                 setStatus(Status.running);
             }
 
-            return doRun(req).then(function (result) {
+            return doRun(req).then(function(result) {
                 if (!ignoreRunStatus) {
                     setStatus(Status.ready);
                 }
-                if (typeof result !== "object") {
-                    return errorMessage(502, "The action did not return a dictionary.");
+                if (typeof result !== 'object') {
+                    return errorMessage(502, 'The action did not return a dictionary.');
                 } else {
                     return responseMessage(200, result);
                 }
-            }).catch(function (error) {
-                var msg = "An error has occurred: " + error;
+            }).catch(function(error) {
+                var msg = 'An error has occurred: ' + error;
                 setStatus(Status.stopped);
                 return Promise.reject(errorMessage(502, msg));
             });
         } else {
-            var msg = "System not ready, status is " + status + ".";
-            console.error("Internal system error:", msg);
+            var msg = 'System not ready, status is ' + status + '.';
+            console.error('Internal system error:', msg);
             return Promise.reject(errorMessage(403, msg));
         }
     };
@@ -146,12 +155,12 @@ function NodeActionService(config) {
     function doInit(message) {
         userCodeRunner = new NodeActionRunner();
 
-        return userCodeRunner.init(message).then(function (result) {
+        return userCodeRunner.init(message).then(function(result) {
             // 'true' has no particular meaning here. The fact that the promise
             // is resolved successfully in itself carries the intended message
             // that initialization succeeded.
             return true;
-        }).catch(function (error) {
+        }).catch(function(error) {
             // emit error to activation log then flush the logs as this
             // is the end of the activation
             console.error('Error during initialization:', error);
@@ -162,23 +171,23 @@ function NodeActionService(config) {
 
     function doRun(req) {
         var msg = req && req.body || {};
-        // Move per-activation keys to process env. vars with __OW_ (reserved) prefix)
+        // Move per-activation keys to process env. vars with __OW_ (reserved) prefix
         Object.keys(msg).forEach(
-            function (k) {
-                if(typeof msg[k] === 'string' && k !== 'value'){
+            function(k) {
+                if (typeof msg[k] === 'string' && k !== 'value'){
                     var envVariable = '__OW_' + k.toUpperCase();
-                    process.env[envVariable] = msg[k];
+                    process.env['__OW_' + k.toUpperCase()] = msg[k];
                 }
             }
         );
 
         return userCodeRunner.run(msg.value).then(function(result) {
-            if (typeof result !== "object") {
+            if (typeof result !== 'object') {
                 console.error('Result must be of type object but has type "' + typeof result + '":', result);
             }
             writeMarkers();
             return result;
-        }).catch(function (error) {
+        }).catch(function(error) {
             console.error(error);
             writeMarkers();
             return Promise.reject(error);
