686acaaa0787bf15bde0f5c0c726d6e9e8c294f2
Rodric Rabbah <rodric@gmail.com>
2020-05-13 18:47:35 -0400

    Add ts runtime to docker images published nightly.

--- tools/travis/publish.sh
+++ tools/travis/publish.sh
@@ -22,23 +22,10 @@ set -eux
 
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 ROOTDIR="$SCRIPTDIR/../.."
-WHISKDIR="$ROOTDIR/../openwhisk"
-
-export OPENWHISK_HOME=$WHISKDIR
 
 IMAGE_PREFIX=$1
 RUNTIME_VERSION=$2
 IMAGE_TAG=$3
-
-if [ ${RUNTIME_VERSION} == "8" ]; then
-  RUNTIME="nodejs8Action"
-elif [ ${RUNTIME_VERSION} == "10" ]; then
-  RUNTIME="nodejs10Action"
-elif [ ${RUNTIME_VERSION} == "12" ]; then
-  RUNTIME="nodejs12Action"
-elif [ ${RUNTIME_VERSION} == "14" ]; then
-  RUNTIME="nodejs14Action"
-fi
 
 if [[ ! -z ${DOCKER_USER} ]] && [[ ! -z ${DOCKER_PASSWORD} ]]; then
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
